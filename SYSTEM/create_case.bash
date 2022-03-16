78a959b1ca41b6e90ed40149ac21c58d9549b6d9
weiwilliam <weiwilliam1987@gmail.com>
2022-03-16 16:37:16 -0400

    Update the Huiying's restructure to wx-aq_v0.1

---
+++ SYSTEM/create_case.bash
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+datpath=${1}
+rundir=${2}
+runpath=${3}
+syspath=${4}
+wpspath=${5}
+wrfpath=${6}
+lbcpath=${7}
+gsipath=${8}
+sdate=${9}
+geo=${10} # should be firstrun
+
+if [ -d $rundir ] 
+then
+ echo "Error: $rundir already exists."
+ exit 1
+else
+  mkdir $rundir
+fi 
+
+mkdir $rundir/wps
+mkdir $rundir/wrf
+mkdir $rundir/gsi
+mkdir $rundir/lbc
+mkdir $rundir/dat
+
+#Link Data
+cd $rundir/dat
+gdasdir="$datpath/gdas/gdas.${sdate:0:8}/${sdate:8:2}"
+gdasln="prepbufr.gdas.${sdate:0:8}.t${sdate:8:2}z.nr"
+gfsdir="$datpath/gfs/gfs.${sdate:0:8}/${sdate:8:2}"
+ln -sf $gdasdir/gdas.* $gdasln
+mkdir gfs
+cd gfs
+#ln -sf $gfsdir/gfs.t${sdate:8:2}z.pgrb2.0p25* .
+ln -sf $gfsdir/* .
+
+
+#WPS
+cd $rundir/wps
+ln -sf $wpspath/Vtable .
+ln -sf $wpspath/link_grib.csh .
+ln -sf $wpspath/ungrib.exe .
+ln -sf $wpspath/metgrid.exe .
+if [ $geo -eq 1 ] # geo output includes data for the whole year
+then
+  ln -sf $wpspath/geogrid.exe .
+  ln -sf $syspath/run_geogrid.sh run_geogrid.sh
+else
+  pdate=`sh ${syspath}/get_pdate.bash $sdate`
+  cp -f $runpath/wrfgsi.run.$pdate/wps/geo_em.* . 
+fi
+ln -sf $syspath/run_ungrib.sh run_ungrib.sh
+ln -sf $syspath/run_metgrid.sh run_metgrid.sh
+
+#WRF
+cd $rundir/wrf
+ln -sf $wrfpath/* .
+ln -sf $syspath/run_real.sh run_real.sh
+ln -sf $syspath/run_wrf.sh run_wrf.sh
+
+#GSI
+cd $rundir/gsi
+mkdir realtime
+ln -sf $gsipath/gsi.x .
+ln -sf $gsipath/comgsi_namelist.sh.soilTQ .
+ln -sf $gsipath/global_convinfo.txt .
+ln -sf $syspath/run_gsi_regional.ksh .
+ln -sf $syspath/datacheck_gsi.sh .
+
+#LBC
+cd $rundir/lbc
+ln -sf $lbcpath/da_update_bc.exe .
+ln -sf $lbcpath/parame.in.LBC parame.in
+ln -sf $syspath/run_lbc.sh .
+
---
+++ SYSTEM/run_lbc.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+JOBNAME="LBC"
+EXE="da_update_bc.exe"
+SCRIPTNAME="${JOBNAME}_runscript"
+NP=1
+JOBSQUEUE="`which squeue` -u ${USER}"
+SQFORMAT="%.10i %.9P %.25j %.8u %.8T %.10M %.10L %.3D %R"
+MPIRUN=`which mpirun`
+APRUN="/usr/bin/time $MPIRUN -np ${NP}"
+
+CKFILE="lateralBC.log"
+
+rundir=${1}
+
+cd $rundir/lbc
+mv $rundir/wrf/wrfbdy_d01 .
+cp -f wrfbdy_d01 wrfbdy_d01.bu
+cp -f $rundir/gsi/realtime/wrf_inout .
+
+
+cat > ./${SCRIPTNAME} << EOF
+#!/bin/bash
+#SBATCH --partition=kratos
+#SBATCH --job-name=${JOBNAME}
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem=96000
+#SBATCH --exclusive
+#SBATCH --time=01:00:00
+ulimit -s unlimited
+$APRUN ${1}/lbc/${EXE} > ${CKFILE} 2>&1
+EOF
+
+sbatch ${1}/lbc/${SCRIPTNAME}
+
+#CHECKPOINT
+sqrc=0
+until [ $sqrc -ne 0 ]
+do
+    $JOBSQUEUE -o "${SQFORMAT}" | grep "$JOBNAME"
+    sqrc=$?
+    sleep 30
+done
+
+grep -i "Successful" $CKFILE >> ${JOBNAME}.log
+ckrc=$?
+if [ $ckrc -eq 1 ]
+then
+    echo Error: Unsuccessfuly run of ${JOBNAME}
+    exit 7
+fi
+
+cp wrfbdy_d01 $rundir/wrf/.
+
+
+#sbatch -p kratos -N1 --exclusive --mem=28000 --wrap="/usr/bin/time mpirun -np 14 /network/rit/home/dg771199/WRF-GSI/src/WPS/geogrid.exe"
+#sbatch -p kratos -N1 --exclusive --mem=28000 --wrap="/usr/bin/time mpirun -np 14 ${1}/geogrid.exe"
